5ecd1d35fc0a11410b4ba04cf042d63a48e8d3c5
guritaalexandru <a_gurita@yahoo.com>
2023-02-10 17:09:19 +0200

    Jenkinsfile modif28

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   // log an env var
-  console.log('here')
+  console.log('here1')
   console.log(process.env.NEXT_PUBLIC_TEST_VAR)
   console.log('here2')
   console.log(process.env.NEXT_PUBLIC_TEST_VAR2)
